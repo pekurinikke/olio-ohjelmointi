e9529c072e45060676f959226013cb825db3a1b7
pekurinikke <hilliniklas@gmail.com>
2025-12-14 22:19:20 +0200

    Lisätty h6b

---
+++ h6b/h6b.pro
@@ -0,0 +1,13 @@
+TEMPLATE = app
+CONFIG += console c++17
+CONFIG -= app_bundle
+
+QT += core
+
+SOURCES += \
+    main.cpp \
+    exampleclass.cpp
+
+HEADERS += \
+    exampleclass.h
+
---
+++ h6b/main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include "exampleclass.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    ExampleClass example;
+    example.startToWait();
+
+    return app.exec();
+}
+
